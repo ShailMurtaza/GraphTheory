24844a9e758fbb12f5ce67bc67cbb1c3db2c076a
ShailMurtaza <shailmurtaza@gmail.com>
2024-12-28 13:39:36 +0500

    Bug Resolved of for not telling shortest path
    First complete all the nodes on current level before adding nodes of
    next level in queue.

--- bi_directional_search.py
+++ bi_directional_search.py
@@ -13,15 +13,18 @@ def intersection(a, b):
 
 # Take data as reference and update it
 def bfs(graph, queue, visited, parent):
-    n = queue.pop(0) # Pop first element of queue
-    for neighbor in graph[n]: # get all child/neighbors of that node from graph
-        if neighbor not in visited: # if this perticular neighbor has not been visited then
-            # add it to queue
-            queue.append(neighbor)
-            # mark it as visisted
-            visited.append(neighbor)
-            # save parent node of node 'n'. It will be used to get path from point of intersection to node
-            parent[neighbor] = n
+    n_queue = []
+    while queue:
+        n = queue.pop(0) # Pop first element of queue
+        for neighbor in graph[n]: # get all child/neighbors of that node from graph
+            if neighbor not in visited: # if this perticular neighbor has not been visited then
+                # add it to queue
+                n_queue.append(neighbor)
+                # mark it as visisted
+                visited.append(neighbor)
+                # save parent node of node 'n'. It will be used to get path from point of intersection to node
+                parent[neighbor] = n
+    return n_queue
 
 
 def bi_dir(graph, start, goal):
@@ -33,13 +36,24 @@ def bi_dir(graph, start, goal):
     parent2 = dict() # parent of every node in backward. It will be used to get full path from start to goal
     intersect = -1 # -1 means not intersected
 
+    print(f"V1: {visited1}")
+    print(f"q1: {queue1}\n")
+    print(f"V2: {visited2}")
+    print(f"q2: {queue2}")
+    print("----------------\n")
     while queue1 and queue2:
-        bfs(graph, queue1, visited1, parent1)
-        bfs(graph, queue2, visited2, parent2)
+        queue1 = bfs(graph, queue1, visited1, parent1)
+        queue2 = bfs(graph, queue2, visited2, parent2)
+        print(f"V1: {visited1}")
+        print(f"q1: {queue1}\n")
+        print(f"V2: {visited2}")
+        print(f"q2: {queue2}")
+        print("----------------\n")
 
         intersect = intersection(visited1, visited2)
         # If intersection of nodes occur then no need to continue
         if intersect != -1:
+            print(intersect)
             break
 
     path = [] # Empty path
@@ -69,8 +83,19 @@ g.add(5, 7)
 g.add(1, 8)
 g.add(8, 9)
 g.add(5, 0)
-start = 9
-goal = 0
+start = 6
+goal = 9
+
+
+# g.add(0, 1)
+# g.add(0, 2)
+# g.add(1, 3)
+# g.add(2, 5)
+# g.add(3, 4)
+# g.add(4, 6)
+# g.add(5, 6)
+# start = 0
+# goal = 6
 
 path = bi_dir(g.graph, start, goal)
 if not path:
@@ -85,4 +110,14 @@ else:
 #   4       5  9
 #  /       / \
 # 6       0   7
+
+
+# { 0: [1, 2], 1: [0, 3], 2: [0, 5], 3: [1, 4], 4: [3, 6], 5: [2, 6], 6: [4, 5] }
+#         0
+#        / \
+#       1   2
+#      /     \
+#     3       5
+#      \      |
+#       4-----6
 
